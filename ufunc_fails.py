6ab624328f364d90eab04cc5a8edd36c3b7be03c
Blake Griffith <blake.a.griffith@gmail.com>
2013-05-09 00:53:18 -0500

    Writing unary tests. Figuring out how this numpy.testing is supposed to work.

--- ufunc_fails.py
+++ ufunc_fails.py
@@ -3,38 +3,50 @@ specification.
 """
 import scipy.sparse as sp
 import numpy as np
-from numpy.testing import assert_equal, assert_almost_equal
+from numpy.testing import assert_almost_equal, assert_equal, assert_raises,\
+    run_module_suite
 
 
 def test_unary_ufuncs():
     """ Test that unary ufuncs act the same on spmatrix objects as
     ndarrays.
     """
-
     # data
     square = np.random.random((4, 4))
-    sp_square = spmatrix(square)
-
-    unary_ufuncs = []
-    unary_with
+    sp_square = sp.csc_matrix(square)
+    unary_ufunc = []
+    for i in dir(np):
+        attr = getattr(np, i)
+        if type(attr) == np.ufunc and attr.nin == 1:
+            unary_ufunc.append(attr)
 
     # ufuncs with one output
     for ufunc in unary_ufunc:
-        sp_val = ufunc(sp_square)
+        # does the ufunc work?
+        try:
+            sp_val = ufunc(sp_square)
+        except AttributeError:
+            assert_raises(AttributeError, ufunc, sp_square)
+            continue
+        except TypeError:
+            assert_raises(TypeError, ufunc, sp_square)
+            continue
         val = ufunc(square)
 
-        # ufuncs that have two outputs.
-        if type(val) == tuple:
-            for sp, dense in zip(sp_val, val):
+        # ufuncs that have two or more outputs.
+        if ufunc.nout != 1:
+            for sparse, dense in zip(sp_val, val):
                 # Type check.
-                assert_equal(type(sp) == sp.spmatrix, True)
-
+                assert_equal(type(sparse) == sp.spmatrix, True)
                 # Equity test.
-                assert_almost_equal(sp.toarray(), dense)
+                assert_almost_equal(sparse.toarray(), dense)
+
         # ufuncs with single outputs.
         else:
             # Type check.
-            assert_equal(type(sp) == sp.spmatrix, True)
-
+            assert_equal(type(sp_val) == sp.csc_matrix, True)
             # Equalit check.
             assert_almost_equal(sp_val.toarray(), val)
+
+if __name__ == '__main__':
+    run_module_suite()
